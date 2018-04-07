a93e14c0c80c992ce035bd3cd58a29f8167f6555
Inam Ul Huq <inamulhuq44@gmail.com>
2018-04-07 21:39:39 +0500

    Create UnauthorizedException.php

---
+++ app/UnauthorizedException.php
@@ -0,0 +1,7 @@
+<?php
+namespace app;
+use Slim\Middleware\TokenAuthentication\UnauthorizedExceptionInterface;
+class UnauthorizedException extends \Exception implements UnauthorizedExceptionInterface
+{
+
+}
